0bab6e00e119dd05f52854e597d2e353471c16d0
ziqian <672699185@qq.com>
2025-04-08 12:39:35 +0800

    使用cookie进行身份验证

---
+++ BlazorAppAuthenticationAndAuthorization/BlazorAppAuthenticationAndAuthorization/Components/Account/AccountComponentsEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace BlazorAppAuthenticationAndAuthorization.Components.Account;
+
+internal static class AccountComponentsEndpointRouteBuilderExtensions
+{
+    public static IEndpointRouteBuilder MapAdditionalAccountEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        ArgumentNullException.ThrowIfNull(endpoints);
+
+        var accountGroup = endpoints.MapGroup("/Account");
+
+        accountGroup.MapPost("/Logout", async (
+             ClaimsPrincipal user,
+             HttpContext httpContext) =>
+        {
+            await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return TypedResults.LocalRedirect("/login");
+        }).RequireAuthorization();
+
+        return endpoints;
+    }
+}
--- BlazorAppAuthenticationAndAuthorization/BlazorAppAuthenticationAndAuthorization/Program.cs
+++ BlazorAppAuthenticationAndAuthorization/BlazorAppAuthenticationAndAuthorization/Program.cs
@@ -1,12 +1,26 @@
 using BlazorAppAuthenticationAndAuthorization.Client.Pages;
 using BlazorAppAuthenticationAndAuthorization.Components;
+using BlazorAppAuthenticationAndAuthorization.Components.Account;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+    .AddInteractiveWebAssemblyComponents()
+    .AddAuthenticationStateSerialization();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";               // 未登录重定向路径
+        options.AccessDeniedPath = "/access-denied"; // 权限不足重定向路径
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // Cookie 有效期
+        options.SlidingExpiration = true;            // 启用滑动过期
+    });
 
 var app = builder.Build();
 
@@ -32,5 +46,8 @@ app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorAppAuthenticationAndAuthorization.Client._Imports).Assembly);
+
+// Add additional endpoints required by the Identity /Account Razor components.
+app.MapAdditionalAccountEndpoints();
 
 app.Run();
